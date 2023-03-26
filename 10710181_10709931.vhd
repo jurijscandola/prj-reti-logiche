536ff73bf210a8fad486e67001350ea6d25d22e1
Daniel Shala <daniel.shala08@gmail.com>
2023-03-26 19:10:24 +0200

    stuck timing implementation

--- 10710181_10709931.vhd
+++ 10710181_10709931.vhd
@@ -52,16 +52,16 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture behavioral of project_reti_logiche is
-    TYPE state_type IS (IDLE, GET_OUT ,GET_ADDR, WAIT_RAM, GET_DATA, WRITE_OUT, DONE);
+    TYPE state_type IS (IDLE, HEADER_1, HEADER_2, GET_ADDR, WAIT_RAM, GET_DATA, WRITE_OUT, DONE);
     SIGNAL state_curr, state_next : state_type;
-    SIGNAL selected_out : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
+    SIGNAL selected_out, selected_out_next : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
     SIGNAL o_mem_addr_next: STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
     SIGNAL mem_reg, mem_reg_next: STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-    SIGNAL count_w : INTEGER RANGE 0 TO 5 := 0;
-    SIGNAL o_z0_next,o_z0_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
-    SIGNAL o_z1_next,o_z1_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
-    SIGNAL o_z2_next,o_z2_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
-    SIGNAL o_z3_next,o_z3_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+    SIGNAL o_z0_next, o_z0_reg, z0_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+    SIGNAL o_z1_next, o_z1_reg, z1_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+    SIGNAL o_z2_next, o_z2_reg, z2_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+    SIGNAL o_z3_next, o_z3_reg, z3_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+    SIGNAL mem_en_next, mem_we_next : STD_LOGIC := '0';
     SIGNAL o_done_next : STD_LOGIC := '0';
 
 BEGIN
@@ -74,61 +74,85 @@ BEGIN
             o_z2 <= "00000000";
             o_z3 <= "00000000";
             o_done <= '0';
+            o_z0_reg <= "00000000";
+            o_z1_reg <= "00000000";
+            o_z2_reg <= "00000000";
+            o_z3_reg <= "00000000";
+
         ELSIF (rising_edge(i_clk)) THEN
             state_curr <= state_next;
             o_mem_addr <= o_mem_addr_next;
+            selected_out <= selected_out_next;
             mem_reg <= mem_reg_next;
             o_done <= o_done_next;
             o_z0 <= o_z0_next;
             o_z1 <= o_z1_next;
             o_z2 <= o_z2_next;
             o_z3 <= o_z3_next;
+            o_z0_reg <= z0_reg;
+            o_z1_reg <= z1_reg;
+            o_z2_reg <= z2_reg;
+            o_z3_reg <= z3_reg;
+
         END IF;
     END PROCESS;
 
-    PROCESS(state_curr, i_start, i_w, mem_reg, i_mem_data, o_z0_reg, o_z1_reg, o_z2_reg, o_z3_reg)
+    PROCESS(state_curr, i_start, i_w, mem_reg, i_mem_data, z0_reg, z1_reg, z2_reg, z3_reg, o_z0_reg, o_z1_reg, o_z2_reg, o_z3_reg, selected_out_next, selected_out)
         BEGIN
+        mem_reg_next <= "0000000000000000";
+        o_mem_addr_next <= "0000000000000000";
+        selected_out_next <= selected_out;
         o_done_next <= '0';
+        o_z0_next <= "00000000";
+        o_z1_next <= "00000000";
+        o_z2_next <= "00000000";
+        o_z3_next <= "00000000";
+        z0_reg <= o_z0_reg;
+        z1_reg <= o_z1_reg;
+        z2_reg <= o_z2_reg;
+        z3_reg <= o_z3_reg;
+        o_mem_we <= '0';
+        o_mem_en <= '0';
         state_next <= state_curr;
 
         CASE state_curr IS
             WHEN IDLE =>
                 IF (i_start = '1') THEN
-                    state_next <= GET_OUT;
+                    selected_out_next(1) <= i_w;
+                    state_next <= HEADER_2;
                 END IF;
 
-            WHEN GET_OUT =>
-                IF count_w < 2 THEN
-                    selected_out <= selected_out(0) & i_w;
-                    count_w <= count_w + 1;
-                ELSE
-                    mem_reg_next <= mem_reg(14 DOWNTO 0) & i_w;
-                    state_next <= GET_ADDR;
-                END IF;
+            WHEN HEADER_1 =>
+                state_next <= HEADER_2;
+
+            WHEN HEADER_2 =>
+                selected_out_next(0) <= i_w;
+                state_next <= GET_ADDR;
 
             WHEN GET_ADDR =>
                 IF (i_start = '0') THEN
                     o_mem_addr_next <= mem_reg;
+                    o_mem_en <= '1';
                     state_next <= WAIT_RAM;
                 ELSE
                     mem_reg_next <= mem_reg(14 DOWNTO 0) & i_w;
                 END IF;
 
             WHEN WAIT_RAM =>
-                o_mem_we <= '0';
                 o_mem_en <= '1';
                 state_next <= GET_DATA;
 
             WHEN GET_DATA =>
+                o_mem_en <= '1';
                 CASE selected_out IS
                     WHEN "00" =>
-                        o_z0_reg <= i_mem_data;
+                        z0_reg <= i_mem_data;
                     WHEN "01" =>
-                        o_z1_reg <= i_mem_data;
+                        z1_reg <= i_mem_data;
                     WHEN "10" =>
-                        o_z2_reg <= i_mem_data;
+                        z2_reg <= i_mem_data;
                     WHEN "11" =>
-                        o_z3_reg <= i_mem_data;
+                        z3_reg <= i_mem_data;
                     WHEN others => null;
                 END CASE;
                 state_next <= WRITE_OUT;
@@ -144,12 +168,8 @@ BEGIN
 
             WHEN DONE =>
                 o_done_next <= '0';
-                count_w <= 0;
                 state_next <= IDLE;
-                o_z0_next <= "00000000";
-                o_z1_next <= "00000000";
-                o_z2_next <= "00000000";
-                o_z3_next <= "00000000";
+                selected_out_next <= "00";
                 o_mem_addr_next <= "0000000000000000";
 
         END CASE;
